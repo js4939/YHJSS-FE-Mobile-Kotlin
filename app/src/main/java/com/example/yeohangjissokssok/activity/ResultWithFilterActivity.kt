830193250818913a88efe899ca5d3d6e985f9767
Mingyu0626 <mg010626@naver.com>
2023-08-25 18:54:32 +0900

    Add | 피그마 기반 사용 예정 액티비티 및 레이아웃 추가 Rename | 액티비티 파일 사용처에 따라 폴더 세분화

---
+++ app/src/main/java/com/example/yeohangjissokssok/activity/ResultWithFilterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yeohangjissokssok.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yeohangjissokssok.R
+
+class ResultWithFilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_with_filter)
+    }
+}
