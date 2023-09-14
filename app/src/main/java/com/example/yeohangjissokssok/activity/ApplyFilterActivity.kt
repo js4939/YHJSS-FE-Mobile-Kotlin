59141f4bd294e229a107d542520f6e0bfe1af669
ohyoonju <oyj1028@konkuk.ac.kr>
2023-09-15 02:21:37 +0900

    Feat | ApplyFilterActivity 클릭 이벤트 기능 구현 (monthText로 선택된 달 넘겨줌)

--- app/src/main/java/com/example/yeohangjissokssok/activity/ApplyFilterActivity.kt
+++ app/src/main/java/com/example/yeohangjissokssok/activity/ApplyFilterActivity.kt
@@ -1,12 +1,39 @@
 package com.example.yeohangjissokssok.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.example.yeohangjissokssok.R
+import kotlin.math.log
 
 class ApplyFilterActivity : AppCompatActivity() {
+
+    private val monthButtons = arrayOf(
+        R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
+        R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8,
+        R.id.btn9, R.id.btn10, R.id.btn11, R.id.btn12
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apply_filter)
+
+        // 월별 버튼에 클릭 리스너 설정
+        for (btnId in monthButtons) {
+            val button = findViewById<Button>(btnId)
+            button.setOnClickListener { onMonthButtonClick(button.text.toString()) }
+        }
+    }
+
+    private fun onMonthButtonClick(monthText: String) {
+        // 선택된 월을 다음 화면으로 전달하고 페이지 변경
+        val intent = Intent(this, ResultWithFilterActivity::class.java)
+        intent.putExtra("selectedMonth", monthText)
+        startActivity(intent)
+
+        Log.d("select month ", monthText)
     }
 }
