cb9604a24cd67ef17b0cbca68db9f1f241ee92a9
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2023-09-14 09:00:45 +0900

    place list api 연결

--- app/src/main/java/com/example/yeohangjissokssok/activity/PlaceListActivity.kt
+++ app/src/main/java/com/example/yeohangjissokssok/activity/PlaceListActivity.kt
@@ -10,6 +10,7 @@ import com.example.yeohangjissokssok.R
 import com.example.yeohangjissokssok.api.RetrofitBuilder
 import com.example.yeohangjissokssok.databinding.ActivityPlaceListBinding
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -36,7 +37,8 @@ class PlaceListActivity : AppCompatActivity() {
     var address = "address"
     var body = ""
 
-    private fun http() {
+
+    private fun getPlaceById() {
         CoroutineScope(Dispatchers.Main).launch {
             RetrofitBuilder.api.getPlace(1).enqueue(object : Callback<APIResponseData> {
                 override fun onResponse(
@@ -44,9 +46,13 @@ class PlaceListActivity : AppCompatActivity() {
                 ) {
                     if (response.isSuccessful) {
                         val temp = response.body() as APIResponseData
-                        val type: Type = object : TypeToken<PlaceResponse>() {}.type
+                        //val type: Type = object : TypeToken<PlaceResponse>() {}.type
                         val jsonResult = Gson().toJson(temp.data)
-                        val result = Gson().fromJson(jsonResult, type) as PlaceResponse
+                        val result: List<PlaceResponse> = GsonBuilder()
+                            .create()
+                            .fromJson(jsonResult, Array<PlaceResponse>::class.java)
+                            .toList()
+                       // val result = Gson().fromJson(jsonResult, type) as PlaceResponse
                         Log.d("result", result.toString())
                     }
                 }
@@ -57,12 +63,34 @@ class PlaceListActivity : AppCompatActivity() {
 
             })
         }
-        data class APIResponseData(
-            val status: Int,
-            val code: String,
-            val message: String,
-            val data: Any
-        ) : java.io.Serializable
+
+    }
+
+    private fun getPlaceByName(input : String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            RetrofitBuilder.api.getPlaceByName(input).enqueue(object : Callback<APIResponseData> {
+                override fun onResponse(
+                    call: Call<APIResponseData>, response: Response<APIResponseData>
+                ) {
+                    if (response.isSuccessful) {
+                        val temp = response.body() as APIResponseData
+                        //val type: Type = object : TypeToken<PlaceResponse>() {}.type
+                        val jsonResult = Gson().toJson(temp.data)
+                        val result: List<PlaceResponse> = GsonBuilder()
+                            .create()
+                            .fromJson(jsonResult, Array<PlaceResponse>::class.java)
+                            .toList()
+                        // val result = Gson().fromJson(jsonResult, type) as PlaceResponse
+                        Log.d("result", result.toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<APIResponseData>, t: Throwable) {
+                    Log.d("test", "연결실패")
+                }
+
+            })
+        }
 
     }
 
@@ -77,7 +105,7 @@ class PlaceListActivity : AppCompatActivity() {
         binding.rvPlace.adapter = placeAdapter
 
         initRecycler()
-        http()
+        getPlaceByName("서울")
     }
 
     private fun initRecycler() {
--- app/src/main/java/com/example/yeohangjissokssok/api/RetrofitService.kt
+++ app/src/main/java/com/example/yeohangjissokssok/api/RetrofitService.kt
@@ -1,11 +1,17 @@
 package com.example.yeohangjissokssok.api
 
 import com.example.yeohangjissokssok.activity.APIResponseData
+import com.example.yeohangjissokssok.activity.PlaceRequest
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RetrofitService {
     @GET("place/{placeId}")
     fun getPlace(@Path("placeId") number : Long) : Call<APIResponseData>
+
+    @GET("place/name")
+    fun getPlaceByName(@Query("name") name: String) : Call<APIResponseData>
 }
