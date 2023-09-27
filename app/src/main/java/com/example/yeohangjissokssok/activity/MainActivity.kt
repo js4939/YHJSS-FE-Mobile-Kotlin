b452c754d295af4311ebd974b2ecf5d0dbe028fd
ohyoonju <oyj1028@konkuk.ac.kr>
2023-09-27 11:30:36 +0900

    Feat | splash -> main 화면 구현

--- app/src/main/java/com/example/yeohangjissokssok/activity/MainActivity.kt
+++ app/src/main/java/com/example/yeohangjissokssok/activity/MainActivity.kt
@@ -10,12 +10,14 @@ import com.example.yeohangjissokssok.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_place_list)
-
-        val intent = Intent(this, PlaceListActivity::class.java)
-        startActivity(intent)
-
-        // 페이지 전환 로그 메시지
-        Log.d("PageTransition", "MainActivity -> PlaceListActivity")
+        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_splash)
+//
+//        //val intent = Intent(this, PlaceListActivity::class.java)
+//        val intent = Intent(this, SplashActivity::class.java)
+//        startActivity(intent)
+//
+//        // 페이지 전환 로그 메시지
+        Log.d("PageTransition", "splash -> main")
     }
 }
